f3d82595f9b54aa5bbb2aa431856ffda785cb732
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2019-03-15 06:49:17 -0400

    Initial Commit

---
+++ OptimumPrime/include/OptimumPrime.h
@@ -0,0 +1,11 @@
+
+#ifndef OPTIMUM_PRIME_INCLUDES_OPTIMUM_PRIME_H_
+#define OPTIMUM_PRIME_INCLUDES_OPTIMUM_PRIME_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <gmp.h>
+
+#endif // OPTIMUM_PRIME_INCLUDES_OPTIMUM_PRIME_H_
+
---
+++ OptimumPrime/src/main.c
@@ -0,0 +1,10 @@
+
+#include "OptimumPrime.h"
+
+
+int main()
+{
+    printf("testing...\n");
+
+    return EXIT_SUCCESS;
+}
